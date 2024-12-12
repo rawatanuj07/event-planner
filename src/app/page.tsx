5b5f6e00bf9ed30f86f0d9116e1ca4bc6db54f6e
Anuj Rawat <neverlandsboi_@Anujs-MacBook-Air.local>
2024-12-12 13:15:11 +0530

    calender-done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
-import Image from "next/image";
 import { generateDate } from "./utils/calender";
 
 export default function Home() {
   console.log("dates are ", generateDate());
-
+  const days = ["S", "M", "T", "W", "T", "F", "S"];
   return (
     <>
-      <h1>hello</h1>
+      <div className="w-96 h-96">
+        <div className="w-full grid grid-cols-7">
+          {days.map((day, index) => {
+            return <h1 className="h-14 grid place-content-center">{day}</h1>;
+          })}
+        </div>
+        <div className="w-full grid grid-cols-7">
+          {generateDate().map(({ date, currentMonth, today }, index) => {
+            return (
+              <div className="h-14 border-t grid place-content-center text-md">
+                <h1
+                  className={`${currentMonth ? "" : "text-gray-500 text-sm"} ${
+                    today ? "bg-red-600 text-white" : ""
+                  } h-10 w-10 grid place-content-center rounded-full hover:bg-white hover:text-black trasnition-all hover:cursor-pointer`}
+                >
+                  {date.date()}
+                </h1>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
--- src/app/utils/calender.ts
+++ src/app/utils/calender.ts
@@ -2,13 +2,32 @@ import dayjs from "dayjs";
 
 export const generateDate = (month = dayjs().month(), year = dayjs().year()) =>{
      
-    const firstDateofMonth = dayjs().year(year).month(month).startOf("month");
-    const lastDateofMonth = dayjs().year(year).month(month).endOf("month");
-    const arrayofDate = [];
-    for (let i: any=firstDateofMonth.date(); i<=lastDateofMonth.date() ; i++){
-        arrayofDate.push(firstDateofMonth.date(i));
+    const firstDateOfMonth = dayjs().year(year).month(month).startOf("month");
+    const lastDateOfMonth = dayjs().year(year).month(month).endOf("month");
+    const arrayOfDate = [];
+
+    // generate prvious month dates
+    // unshift(): Adds these dates at the start of the array in reverse order 
+    // so they appear before the first day of the month in the calendar grid.
+    for (let i = 0; i < firstDateOfMonth.day(); i++) {
+        const date = firstDateOfMonth.subtract(i + 1, "day");    
+        arrayOfDate.unshift({date: date, currentMonth: false});
     }
-    return arrayofDate;
+
+    // generate current month dates
+    for (let i: any=firstDateOfMonth.date(); i<=lastDateOfMonth.date() ; i++){
+        arrayOfDate.push({date: firstDateOfMonth.date(i), currentMonth: true, today: firstDateOfMonth.date(i).toDate().toDateString()===dayjs().toDate().toDateString()
+        });
+    }
+
+    //generate rest of the dates
+	const remaining = 42 - arrayOfDate.length;
+
+    for (let i: any = lastDateOfMonth.date()+1; i<=lastDateOfMonth.date()+remaining; i++){
+        arrayOfDate.push({date: lastDateOfMonth.date(i), currentMonth: false});
+
+    }
+    return arrayOfDate;
 
 
 }
